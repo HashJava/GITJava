f71355fe0e129456340487d12ce3c0608a364e81
Hash <hashshahijava@gmail.com>
2022-10-12 16:08:02 -0400

    First commit made

---
+++ src/GitTry/git2.java
@@ -0,0 +1,19 @@
+package GitTry;
+
+public class git2 {
+
+	public static void main(String[] args) {
+
+		String MastersDegree = "You need to work hard";
+
+		if (MastersDegree == "You need to work hard") {
+
+			System.out.println("You will achive your goal");
+		} else
+			System.out.println("Else you fail");
+		
+		
+		
+	}
+
+}
